09a90f0156758a331b8c3745ac7a86270c775125
dyarfaradj <dyar.98@hotmail.com>
2019-10-08 23:52:01 +0200

    init project asp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Distribuerade_System_Labb_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Distribuerade-System-Labb-2
+ Distribuerade-System-Labb-2
